bf069125370e6a653b70d17f6f661a42132faf18
HelenFedorova <nvihod@bk.ru>
2025-08-11 20:08:02 +0300

    add in Tasks file

--- Lesson 7. Files.py
+++ Lesson 7. Files.py
@@ -15,9 +15,41 @@ with open("greeting.txt", "r", encoding='utf-8') as file:
 # Затем прочитай и выведи только вторую строку из этого файла.
 
 with open("greeting.txt", "w", encoding='utf-8') as file:
-    file.write("Первая строка"\n)
-    file.write("Вторая строка"\n)
-    file.write("Третья строка"\n)
+    file.write("Первая строка\n")
+    file.write("Вторая строка\n")
+    file.write("Третья строка\n")
+
 with open("greeting.txt", "r", encoding='utf-8') as file:
     lines = file.readlines()
-    print(lines[1].strip())
+    print(lines[1].strip())  # Выводим вторую строку (индекс 1)
+
+# 4
+# Создай файл 'greeting.txt' и запиши в него одну строку: "Привет, Python!"
+# Прочитай файл и выведи его содержимое в консоль
+# Используй только один контекстный менеджер with
+
+with open("greeting.txt", "w", encoding='utf-8') as file:
+    file.write("Привет, Python!")
+
+with open("greeting.txt", "r", encoding='utf-8') as file:
+    lines = file.readlines()
+    print(lines)
+
+# 5
+# Создай файл 'greeting.txt' и запиши в него своё имя
+# Выведи сообщение: "Файл создан! Проверь его содержимое."
+# (Читать файл не нужно)
+
+with open("greeting.txt", "w", encoding='utf-8') as file:
+    file.write("Helena")
+
+print("Файл создан! Проверь его содержимое.")
+
+# 6
+
+# Создай файл 'greeting.txt' и запиши в него одну цифру: 5
+# (Выводить ничего не нужно)
+
+with open("greeting.txt", "w", encoding='utf-8') as file:
+    file.write("5")
+
